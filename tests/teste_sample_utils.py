ec455c9aa32fda23256cf74bbc11762c53fc8338
Fabiana Zioti <fabiana_zioti@hotmail.com>
2021-02-24 09:56:34 -0300

    adding drone integration ref #10

---
+++ tests/teste_sample_utils.py
@@ -0,0 +1,15 @@
+#
+# This file is part of Sample Database Utils.
+# Copyright (C) 2020-2021 INPE.
+#
+# Sample Database Utils is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+
+"""Unit-test for Sample Database Utils."""
+
+
+class TestSampleUtils:
+
+    def test_factory(self):
+        pass
